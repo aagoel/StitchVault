b3a32229f6a9accacdf4855e3cbda4ba8cafeca1
Aarti Goel <aartigoelxii@gmail.com>
2023-06-24 15:48:18 +0530

    making changes

--- lib/user.dart
+++ lib/user.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'appbar.dart';
-import 'edit_user.dart';
 import 'add_user.dart';
+
+const List<String> list = <String>[
+  'Employee 1',
+  'Employee 2',
+  'Employee 3',
+  'Employee 4'
+];
 
 class User extends StatefulWidget {
   const User({super.key});
@@ -13,8 +19,14 @@ class User extends StatefulWidget {
 class _UserBottomNavigationBarExampleState extends State<User> {
   int _selectedIndex = 0;
   final String title = "User";
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   final List<Widget> _widgetOptions = <Widget>[
     const DropdownButtonExample(),
+    const Text(
+      'Home Page',
+      style: optionStyle,
+    ),
     LoginPage(),
   ];
 
@@ -50,6 +62,43 @@ class _UserBottomNavigationBarExampleState extends State<User> {
         selectedItemColor: Colors.blue[800],
         onTap: _onItemTapped,
       ),
+    );
+  }
+}
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
